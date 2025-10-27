a8aa4f345b38466a6c15b18d574ebf0d6141b53e
elika <elika95589@gmail.com>
2025-10-27 23:10:31 +0200

    auth

--- src/server.js
+++ src/server.js
@@ -3,21 +3,25 @@ import cors from "cors";
 import dotenv from 'dotenv';
 import { connectMongoDB } from "./db/connectMongoDB.js";
 import notesRouter from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 import { logger } from './middleware/logger.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errors } from 'celebrate';
+import cookieParser from 'cookie-parser';
+
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3000;
 
 app.use(logger);
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
-
+app.use(authRoutes);
 app.use(notesRouter);
 
 app.use(notFoundHandler);
