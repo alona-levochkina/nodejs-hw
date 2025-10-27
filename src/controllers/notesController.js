54e36d4a42ce4b5912f94a204a94ab7e44cc54b1
elika <elika95589@gmail.com>
2025-10-27 23:33:33 +0200

    fix notesController

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -5,9 +5,8 @@ export const getAllNotes = async (req, res, next) => {
   const { page = 1, perPage = 10, tag, search } = req.query;
 
   const skip = (page - 1) * perPage;
-  const userId = req.user._id;
 
-  let notesQuery = Note.find({ userId });
+  let notesQuery = Note.find({ userId: req.user._id });
 
   if (tag) {
     notesQuery = notesQuery.where('tag').equals(tag);
@@ -35,9 +34,8 @@ export const getAllNotes = async (req, res, next) => {
 
 export const getNoteById = async (req, res, next) => {
   const { noteId } = req.params;
-  const userId = req.user._id;
 
-  const note = await Note.findById({ _id: noteId, userId });
+  const note = await Note.findOne({ _id: noteId, userId: req.user._id, });
 
   if (!note) {
     next(createHttpError(404, 'Note not found'));
@@ -48,16 +46,14 @@ export const getNoteById = async (req, res, next) => {
 };
 
 export const createNote = async (req, res, next) => {
-  const userId = req.user._id;
-  const newNote = await Note.create({ ...req.body, userId });
+  const newNote = await Note.create({ ...req.body, userId: req.user._id });
 
   res.status(201).json(newNote);
 };
 
 export const deleteNote = async (req, res, next) => {
   const { noteId } = req.params;
-  const userId = req.user._id;
-  const deletedNote = await Note.findByIdAndDelete({ _id: noteId, userId });
+  const deletedNote = await Note.findOneAndDelete({ _id: noteId, userId: req.user._id, });
 
   if (!deletedNote) {
     next(createHttpError(404, 'Note not found'));
@@ -69,8 +65,7 @@ export const deleteNote = async (req, res, next) => {
 
 export const updateNote = async (req, res, next) => {
   const { noteId } = req.params;
-    const userId = req.user._id;
-  const updatedNote = await Note.findByIdAndUpdate({ _id: noteId, userId }, req.body, {
+  const updatedNote = await Note.findOneAndUpdate({ _id: noteId, userId: req.user._id }, req.body, {
     new: true,
   });
 
